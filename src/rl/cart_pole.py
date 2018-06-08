905651f3ab4885f496f2c4e40a6e16590f6984ff
Krzysztof Langner <klangner@gmail.com>
2018-06-08 15:53:49 +0200

    params

--- src/rl/cart_pole.py
+++ src/rl/cart_pole.py
@@ -1,4 +1,8 @@
-# Solved CartPole using DQN
+# Solved CartPole using DQN (Policy Gradient)
+#
+# This is minimal introduction to the DQN learning.
+# The agent should solve the environment in ~1K episodes.
+# But still it is not perfect. Sometimes it fails.
 #
 # https://gym.openai.com/envs/CartPole-v0/
 #
@@ -18,12 +22,13 @@ MAX_STEPS = 200
 SOLVED_SCORE = 200  # 190
 
 
-start_time = time.time()
-env = gym.make("CartPole-v0").env
-network = QNetwork(env.observation_space.shape, env.action_space.n)
-agent = DQNAgent(network, env.action_space.n)
-rewards = solve_env(env, agent, max_sessions=MAX_SESSIONS, t_max=MAX_STEPS, solved=SOLVED_SCORE)
-end_time = time.time()
-print('Finished in {} seconds'.format(int(end_time-start_time)))
-plt.plot(rewards)
-plt.show()
+if __name__ == '__main__':
+    start_time = time.time()
+    env = gym.make("CartPole-v0").env
+    network = QNetwork(env.observation_space.shape, env.action_space.n)
+    agent = DQNAgent(network, env.action_space.n)
+    rewards = solve_env(env, agent, max_sessions=MAX_SESSIONS, t_max=MAX_STEPS, solved=SOLVED_SCORE)
+    end_time = time.time()
+    print('Finished in {} seconds'.format(int(end_time-start_time)))
+    plt.plot(rewards)
+    plt.show()
